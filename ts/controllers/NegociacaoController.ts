23fd066a9c9504ad5376fc6ac664927758831d59
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-23 13:56:52 -0300

    corrigida tipagem de variáveis

--- ts/controllers/NegociacaoController.ts
+++ ts/controllers/NegociacaoController.ts
@@ -1,22 +1,22 @@
 class NegociacaoController{
 
-  private _inputData;
-  private _inputQuantidade;
-  private _inputValor;
+  private _inputData: HTMLInputElement;
+  private _inputQuantidade: HTMLInputElement;
+  private _inputValor: HTMLInputElement;
 
   constructor(){
-    this._inputData =  document.getElementById("data");
-    this._inputQuantidade =  document.getElementById("quantidade");
-    this._inputValor =  document.getElementById("valor");
+    this._inputData =  <HTMLInputElement>document.getElementById("data");
+    this._inputQuantidade =  <HTMLInputElement> document.getElementById("quantidade");
+    this._inputValor =  <HTMLInputElement> document.getElementById("valor");
   }
 
-  adiciona(evento){
+  adiciona(evento: Event){
     evento.preventDefault();
 
     const negociacao = new Negociacao(
-                          this._inputData.value,
-                          this._inputQuantidade.value,
-                          this._inputValor.value
+                          new Date(this._inputData.value.replace(/-/g, ',')),
+                          parseInt(this._inputQuantidade.value),
+                          parseFloat(this._inputValor.value)
                         );
     console.log(negociacao);
   }
