18df93f617a48212e8590148f9ffcad763694837
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-23 15:01:29 -0300

    atualizada documentação

--- ts/models/Negociacoes.ts
+++ ts/models/Negociacoes.ts
@@ -1,15 +1,34 @@
+/**
+ * @namespace ts/models/Negociacoes
+ * @description encapsula array de negociações
+ */
 class Negociacoes{
   private _negociacoes: Negociacao[] = [];
 
+  /**
+   * @access public
+   * @param Negociacao negociacao
+   * @description adiciona negociacao ao array
+   * @return Negociacoes this
+   */
   adiciona(negociacao: Negociacao){
     this._negociacoes.push(negociacao);
     return this;
   }
 
+  /**
+   * @access public
+   * @return Array negociacoes
+   */
   toArray(){
     return this._negociacoes;
   }
 
+  /**
+   * @access public
+   * @description esvazia objeto
+   * @return Negociacoes this
+   */
   esvazia(){
     this._negociacoes = [];
     return this;
