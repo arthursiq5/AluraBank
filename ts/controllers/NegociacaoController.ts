3e6fe4f8253ff1043f10b1543edee23fe81785f5
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-23 14:06:29 -0300

    corrigida formatação

--- ts/controllers/NegociacaoController.ts
+++ ts/controllers/NegociacaoController.ts
@@ -6,7 +6,9 @@ class NegociacaoController{
 
   constructor(){
     this._inputData =  <HTMLInputElement>document.getElementById("data");
+
     this._inputQuantidade =  <HTMLInputElement> document.getElementById("quantidade");
+
     this._inputValor =  <HTMLInputElement> document.getElementById("valor");
   }
 
@@ -14,10 +16,13 @@ class NegociacaoController{
     evento.preventDefault();
 
     const negociacao = new Negociacao(
-                          new Date(this._inputData.value.replace(/-/g, ',')),
-                          parseInt(this._inputQuantidade.value),
-                          parseFloat(this._inputValor.value)
-                        );
+                        new Date(
+                          this._inputData.value.replace(/-/g, ',')
+                        ),
+                        parseInt(this._inputQuantidade.value),
+                        parseFloat(this._inputValor.value)
+                       );
+                       
     console.log(negociacao);
   }
 }
