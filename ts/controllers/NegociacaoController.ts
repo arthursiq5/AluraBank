02632423d57f42980bbbd47357db4de2e93891e0
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-23 14:13:35 -0300

    atualizada documentação

--- ts/controllers/NegociacaoController.ts
+++ ts/controllers/NegociacaoController.ts
@@ -1,9 +1,18 @@
+/**
+ * @namespace ts/controllers/NegociacaoController
+ */
 class NegociacaoController{
 
+  /** @var HTMLInputElement _inputData */
   private _inputData: HTMLInputElement;
+  /** @var HTMLInputElement _inputQuantidade */
   private _inputQuantidade: HTMLInputElement;
+  /** @var HTMLInputElement _inputValor */
   private _inputValor: HTMLInputElement;
 
+  /**
+   * @constructor
+   */
   constructor(){
     this._inputData =  <HTMLInputElement>document.getElementById("data");
 
@@ -12,6 +21,12 @@ class NegociacaoController{
     this._inputValor =  <HTMLInputElement> document.getElementById("valor");
   }
 
+  /**
+   * @access public
+   * @param Event evento
+   * @description adiciona uma negociação no banco
+   * @returns NegociacaoController this
+   */
   adiciona(evento: Event){
     evento.preventDefault();
 
@@ -22,7 +37,9 @@ class NegociacaoController{
                         parseInt(this._inputQuantidade.value),
                         parseFloat(this._inputValor.value)
                        );
-                       
+
     console.log(negociacao);
+
+    return this;
   }
 }
--- ts/models/Negociacao.ts
+++ ts/models/Negociacao.ts
@@ -1,23 +1,48 @@
+/**
+ * @namespace js/models/Negociacao
+ */
 class Negociacao{
 
+  /**
+   * @constructs
+   * @param Date data
+   * @param number quantidade
+   * @param number valor
+   */
   constructor(
     private _data: Date,
     private _quantidade: number,
     private _valor: number
   ){}
 
+  /**
+   * @access public
+   * @return Date data
+   */
   get data(){
     return this._data;
   }
 
+  /**
+   * @access public
+   * @return number quantidade
+   */
   get quantidade(){
     return this._valor;
   }
 
+  /**
+   * @access public
+   * @return number valor
+   */
   get valor(){
     return this._valor;
   }
 
+  /**
+   * @access public
+   * @return number valor
+   */
   get volume(){
     return this.quantidade * this._valor;
   }
