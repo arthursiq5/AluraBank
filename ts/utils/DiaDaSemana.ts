1717bd0db61fd39677536f65ff889b63d0189356
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-27 13:44:43 -0300

    criada opção de uso de enumeradores em caixa alta

--- ts/utils/DiaDaSemana.ts
+++ ts/utils/DiaDaSemana.ts
@@ -6,5 +6,12 @@ export enum DiaDaSemana{
   Quarta = 3,
   Quinta = 4,
   Sexta = 5,
-  Sabado = 6
+  Sabado = 6,
+  DOMINGO = 0,
+  SEGUNDA = 1,
+  TERCA = 2,
+  QUARTA = 3,
+  QUINTA = 4,
+  SEXTA = 5,
+  SABADO = 6
 }
